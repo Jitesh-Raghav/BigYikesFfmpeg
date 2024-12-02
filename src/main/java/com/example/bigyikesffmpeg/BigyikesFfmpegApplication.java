72a4dccf533c55b612a7d0bffa8851dc8ca883ca
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-12-02 22:21:24 +0530

    init

---
+++ src/main/java/com/example/bigyikesffmpeg/BigyikesFfmpegApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigyikesffmpeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigyikesFfmpegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigyikesFfmpegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigyikesffmpeg/BigyikesFfmpegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigyikesffmpeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigyikesFfmpegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
