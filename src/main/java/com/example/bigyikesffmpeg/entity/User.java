65919aa741b29d7dd73ad2fd2e2792fb402e8780
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-12-02 22:31:47 +0530

    added user entity

--- src/main/java/com/example/bigyikesffmpeg/entity/User.java
+++ src/main/java/com/example/bigyikesffmpeg/entity/User.java
@@ -1,4 +1,24 @@
 package com.example.bigyikesffmpeg.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
 }
